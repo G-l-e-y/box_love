431258a3fadd04ba7e41a464d6dae9493287d164
Gleidson ''Gley'' Henrique <132686774+G-l-e-y@users.noreply.github.com>
2024-07-08 11:54:29 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Box Love",
+
+    background: "background.jpg",
+
+    min1: "1mi.webp",
+    min2: "2mi.webp",
+    min3: "3mi.webp",
+    min4: "4mi.jpg",
+    min5: "5mi.jpg",
+    min6: "6mi.webp",
+
+    max1: "1ma.jpg",
+    max2: "2ma.webp",
+    max3: "3ma.webp",
+    max4: "4ma.jpg",
+    max5: "5ma.webp",
+    max6: "6ma.jpg",
+}
